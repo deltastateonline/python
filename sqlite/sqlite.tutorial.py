694e3c743525706bc6016c02844c5f12a6a69029
deltastateonline <joomla@deltastateonline.com>
2016-07-31 07:38:45 +1000

    Switch to an orm

--- sqlite/sqlite.tutorial.py
+++ sqlite/sqlite.tutorial.py
@@ -23,7 +23,7 @@ def main():
     db = sqlite3.connect('data/mydb.sqlite') 
     
     doWhat = "";
-    #createDatabase(db)
+   
     while doWhat != "Q":
         doWhat = readInput()
         doWhat = doWhat.upper()
@@ -57,11 +57,7 @@ def createDatabase(db):
     # Get a cursor object
     print "Creating Database ...."
     try:
-        cursor = db.cursor()
-        cursor.execute('''
-            CREATE TABLE carlist(id INTEGER PRIMARY KEY, carMake TEXT,price real)
-        ''')
-        db.commit()
+       
     except BaseException:
         print("Something went wrong!  Try again...")
     
@@ -70,9 +66,7 @@ def deleteDatabase(db):
     # Get a cursor object
     print "Deleting Database ...."
     try:
-        cursor = db.cursor()
-        cursor.execute('''DROP TABLE carlist''')
-        db.commit()
+       
     except BaseException:
         print("Something went wrong!  Try again...")
     
@@ -80,13 +74,7 @@ def insertRecord(db):
       # Get a cursor object
     print "Insert Records ...."
     try:
-        cursor = db.cursor()
-        carData = readDetails()
-        print carData
-        cursor.execute('''INSERT INTO carlist(price,carMake)
-                  VALUES(:price,:carMake)''',carData)                       
-                        
-        db.commit()
+       
     except BaseException as inst:
         print inst
         print("Something went wrong!  Try again...")
@@ -95,16 +83,7 @@ def insertRecord(db):
 def readRecords(db):
     print "Reading Records ...."
     try:
-        db.row_factory = sqlite3.Row
-        cursor = db.cursor()
        
-        cursor.execute('''select * from carlist''')                       
-        allRecords = cursor.fetchall()
-        for x in allRecords:
-            print "%4d %s\t| %10.2f"%(x["id"],x["carMake"],x["price"])
-           
-               
-        db.commit()
     except BaseException as inst:
         print inst
         print("Something went wrong!  Try again...")
@@ -114,18 +93,7 @@ def findRecord(db):
     print "Finding a record.."
     carMake = input("Enter the Car Make..")
     try:
-        db.row_factory = sqlite3.Row
-        cursor = db.cursor()
-       
-        cursor.execute('''select * from carlist where carMake like ?''',('%'+carMake+'%',))                       
-        allRecords = cursor.fetchall()
-        if allRecords :
-        	for x in allRecords:
-        		print "%4d  %s\t| %10.2f"%(x["id"],x["carMake"],x["price"])
-        else:
-         	 print "Unable to find record for search term {0}".format(carMake)
-               
-        db.commit()
+        
     except BaseException as inst:
         print inst
         print("Something went wrong!  Try again...")
@@ -135,21 +103,7 @@ def delRecord(db):
     print "Delete a record.."
     carMake = input("Enter the Car Make..")
     try:
-        db.row_factory = sqlite3.Row
-        cursor = db.cursor()
-       
-        cursor.execute('''select * from carlist where carMake like ?''',('%'+carMake+'%',))                      
-        allRecords = cursor.fetchall()
-        if allRecords :
-        	for x in allRecords:
-        		print "%4d  %s\t| %10.2f"%(x["id"],x["carMake"],x["price"])
-        		deleteRec = input("Delete? (Y)es or (N)o ..")
-        		if(deleteRec.upper() == "Y"):
-        			cursor.execute('''delete from carlist where id = ?''',(x["id"],)) 
-        else:
-         	 print "Unable to find record for search term {0}".format(carMake)
-               
-        db.commit()
+        
     except BaseException as inst:
         print inst
         print("Something went wrong!  Try again...")   
